9d1c544fa65db33c15ed9bc76a9c91b3e37d4b14
an-2-an <rjabenkoae@gmail.com>
2022-02-17 10:05:59 +0200

    etap two

--- simplified_markdown_editor.py
+++ simplified_markdown_editor.py
@@ -1,6 +1,7 @@
 FORMATTERS = ['plain', 'bold', 'italic', 'header', 'link', 'inline-code', 'ordered-list', 'unordered-list', 'new-line']
 COMMANDS = ['!help', '!done']
 MSG_UNKNOWN = 'Unknown formatting type or command'
+content = []
 
 def get_formatter():
     return input('Choose a formatter: ')
@@ -9,6 +10,48 @@ def show_help():
     text = '''Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line
 Special commands: !help !done'''
     print(text)
+
+def show_content():
+    for line in content:
+        print(line)
+
+def formatting(formatter):
+    # plain
+    if formatter == FORMATTERS[0]:
+        line = input('Text: ')
+        content.append(line)
+    # bold
+    if formatter == FORMATTERS[1]:
+        line = input('Text: ')
+        content.append(f'**{line}**')
+    # italic
+    if formatter == FORMATTERS[2]:
+        line = input('Text: ')
+        content.append(f'*{line}*')
+    # header
+    if formatter == FORMATTERS[3]:
+        level = int(input('Level: '))
+        line = input('Text: ')
+        content.append('#' * level + ' ' + line)
+    # link [title](https://www.example.com)
+    if formatter == FORMATTERS[4]:
+        label = input('Label: ')
+        link = input('URL: ')
+        content.append(f'[{label}]({link})')
+    # inline-code
+    if formatter == FORMATTERS[5]:
+        line = input('Text: ')
+        content.append(f'`{line}`')
+    # ordered-list
+    pass
+    # unordered-list
+    pass
+    # new-line
+    if formatter == FORMATTERS[8]:
+        content.append('')
+
+    show_content()
+
 
 def main():
     while True:
@@ -20,6 +63,8 @@ def main():
                 break
             if f == COMMANDS[0]:
                 show_help()
+            else:
+                formatting(f)
 
 
 if __name__ == '__main__':
